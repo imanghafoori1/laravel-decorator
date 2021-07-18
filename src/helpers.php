2ea772e7b995a2253f7eb177e69fd72ec7bb75e7
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-19 00:15:54 +0430

    Add get_func_argNames & get_method_argNames in helpers

--- src/helpers.php
+++ src/helpers.php
@@ -5,3 +5,32 @@ if (!function_exists('callWithDecorators')) {
     {
     }
 }
+if (!function_exists("get_func_argNames")) {
+    /**
+     * @throws ReflectionException
+     */
+    function get_func_argNames($funcName)
+    {
+        $f = new ReflectionFunction($funcName);
+        $result = array();
+        foreach ($f->getParameters() as $param) {
+            $result[] = $param->name;
+        }
+        return $result;
+    }
+}
+
+if (!function_exists("get_method_argNames")) {
+    /**
+     * @throws ReflectionException
+     */
+    function get_method_argNames($className, $methodName)
+    {
+        $f = new ReflectionMethod($className, $methodName);
+        $result = array();
+        foreach ($f->getParameters() as $param) {
+            $result[] = $param->name;
+        }
+        return $result;
+    }
+}
